8b4c5b96b1396946c74640c6bfabec9673fba8c0
“Enter <anirudhanand984@gmail.com>
2021-11-09 18:54:15 +0530

    [Anirudh] Add : created UC3-EmpPartTimeFullTime branch

--- EmpWages/EmployeeWageComputation.cs
+++ EmpWages/EmployeeWageComputation.cs
@@ -6,20 +6,27 @@ namespace EmpWages
 {
     class EmployeeComputation
     {
-        public const int is_Present = 1, WAGE_PER_HOUR = 20, FULL_WORKING_HOURS = 8;
-        int totalEmpWage;
-        public void DailyEmployeeWage()
+        public const int IS_FULL_TIME = 1, IS_PART_TIME = 2, WAGE_PER_HOUR = 20, FULL_WORKING_HOURS = 8, PART_TIME_HRS = 4;
+        int totalEmpWage, empHrs;
+        public void FULL_TIME_PART_TIME()
         {
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == is_Present)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_FULL_TIME)
             {
-                this.totalEmpWage = FULL_WORKING_HOURS * WAGE_PER_HOUR;
-                Console.WriteLine("employee present" + this.totalEmpWage);
+                this.empHrs = FULL_WORKING_HOURS;
+
+            }
+            if (empCheck == IS_PART_TIME)
+            {
+                this.empHrs = PART_TIME_HRS;
             }
 
             else
-                Console.WriteLine("employee Absent");
+            {
+                this.empHrs = 0;
+            }
+            this.totalEmpWage = this.empHrs * WAGE_PER_HOUR;
 
         }
 
