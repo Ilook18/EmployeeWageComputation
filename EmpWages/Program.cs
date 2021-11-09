162a7482514fcb33952de1b910cd594ea7de3c91
“Enter <anirudhanand984@gmail.com>
2021-11-09 18:17:39 +0530

    [Anirudh] Add : created EmployeeWageCompuataion Problem

---
+++ EmpWages/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmpWages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee wage Computation problem");
+            
+
+        }
+    }
+}
