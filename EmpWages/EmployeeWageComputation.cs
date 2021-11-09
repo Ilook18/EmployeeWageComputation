bf36d6c351897f2cedfaf405219727469992ba54
“Enter <anirudhanand984@gmail.com>
2021-11-09 18:44:11 +0530

    [Anirudh] Add : created branch UC2-DailyEmployeeWage

--- EmpWages/EmployeeWageComputation.cs
+++ EmpWages/EmployeeWageComputation.cs
@@ -6,13 +6,18 @@ namespace EmpWages
 {
     class EmployeeComputation
     {
-        public const int is_Present = 1;
-        public void EmployeeAttendence()
+        public const int is_Present = 1, WAGE_PER_HOUR = 20, FULL_WORKING_HOURS = 8;
+        int totalEmpWage;
+        public void DailyEmployeeWage()
         {
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == is_Present)
-                Console.WriteLine("employee present");
+            {
+                this.totalEmpWage = FULL_WORKING_HOURS * WAGE_PER_HOUR;
+                Console.WriteLine("employee present" + this.totalEmpWage);
+            }
+
             else
                 Console.WriteLine("employee Absent");
 
