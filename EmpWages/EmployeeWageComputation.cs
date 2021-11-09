a4d43698d6afa3ccf36c592c7229defede20de8c
“Enter <anirudhanand984@gmail.com>
2021-11-09 18:23:40 +0530

    [Anirudh] Add : created Branch UC1-Attendence

---
+++ EmpWages/EmployeeWageComputation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmpWages
+{
+    class EmployeeComputation
+    {
+        public const int is_Present = 1;
+        public void EmployeeAttendence()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == is_Present)
+                Console.WriteLine("employee present");
+            else
+                Console.WriteLine("employee Absent");
+
+        }
+
+    }
+}
+
